2190b466c5753585e5d95f48dcfb878217e0191a
mskwon1 <mageeeeek@gmail.com>
2024-02-14 23:17:57 +0900

    Adds IndexPage

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,3 +1,22 @@
+import { css } from '@emotion/css';
+import { Link } from 'react-router-dom';
+
 export default function IndexPage() {
-  return <div>Hello World</div>;
+  return (
+    <div
+      className={css({
+        display: 'flex',
+        flexDirection: 'column',
+        padding: 4,
+        gap: 8,
+      })}
+    >
+      <h1>Todo page using ...</h1>
+      <ul>
+        <li>
+          <Link to="/zustand">Zustand</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
