736673ca100209a3bda196a445a930f0f0f812fd
mskwon1 <mageeeeek@gmail.com>
2024-02-14 21:18:33 +0900

    Adds update todo action

--- src/prototypes/ZustandTodoPage/useZustandTodo.ts
+++ src/prototypes/ZustandTodoPage/useZustandTodo.ts
@@ -3,10 +3,19 @@ import { create } from 'zustand';
 const useZustandTodo = create<{
   todos: Todo[];
   addTodo: (todo: Todo) => void;
+  updateTodo: (todo: Todo) => void;
   removeTodo: (id: string) => void;
 }>((set) => ({
   todos: [],
   addTodo: (todo) => set((state) => ({ todos: [...state.todos, todo] })),
+  updateTodo: (todo) =>
+    set((state) => {
+      return {
+        todos: state.todos.map((prevTodo) => {
+          return prevTodo.id === todo.id ? todo : prevTodo;
+        }),
+      };
+    }),
   removeTodo: (id) =>
     set((state) => ({ todos: state.todos.filter((todo) => todo.id !== id) })),
 }));
